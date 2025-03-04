df4c59892b5b2c16abee264aa465268ac2ca40e2
Colin <colinmcnallyj@gmail.com>
2025-03-03 23:21:03 -0500

    Initializing

---
+++ react_app/src/Message.tsx
@@ -0,0 +1,10 @@
+
+//PascalCasing: Capitalising the first letter of each word in the name of the file. 
+function Message() {
+    //JSX: JavaScript XML
+    const name = 'Colin';
+    return <h1>Hello {name}</h1>
+
+}
+
+export default Message
