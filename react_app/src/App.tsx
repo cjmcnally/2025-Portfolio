21533b117e52c42a72478de73744b155c3f05a20
Colin <colinmcnallyj@gmail.com>
2025-03-04 00:02:37 -0500

    Preparing Home Structure
    Using Figma for designing elements & structure

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Message from './Message'
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
-  return <div><Message /></div>;
+  // Structure of App
+
+  /*
+    1. Navbar At Top Centered
+    2. Title & Picture of Myself
+    3. Button Scrolls Down to Projects
+    4. Projects Section: 3 Projects, 3 seperate containers
+    5. About Me Section, include Skills section next to it
+    6. Footer with Contact Information
+  */
+
+  // 1. Navbar At Top Centered
+  return (
+  <nav>
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/about">About Me</a>
+      </li>
+      <li>
+        <a href="/projects">My Projects</a>
+      </li>
+      <li>
+        <a href="/contact">Contact Me</a>
+      </li>
+    </ul>
+  </nav>
+  );
+    <h1>
+      <Title />
+    </h1>
 }
 
-export default App
+export default App;
---
+++ react_app/src/components/Title.tsx
@@ -0,0 +1,7 @@
+
+
+function Title() {
+    return <h1>Welcome To My Portfolio!</h1>
+}
+
+export default Title;
